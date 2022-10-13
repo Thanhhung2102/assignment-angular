9e4d34febf7ad6e6f7a708c054e91ce2739589cb
Hungttph13998 <hungttph13998@fpt.edu.vn>
2022-10-13 12:42:20 +0700

    refactor: currency VND

--- src/app/components/display/home-page/home-page.component.ts
+++ src/app/components/display/home-page/home-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
+  products!: IProduct[];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
   }
-
 }
